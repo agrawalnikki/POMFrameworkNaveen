7c52a943afd9cd15dd08c9b97e5a1ae86a1908ad
nikita agrawalpwddc <agrawal.nikita0688@gmail.com>
2023-07-30 17:40:23 -0500

    mail nakul

---
+++ src/main/java/com/qa/opencart/pages/Mail.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class Mail {
+
+	public void sendMail() {
+		System.out.println("Mail sent");
+		System.out.println("Nakul's code");
+	}
+	
+	public void getMail() {
+		System.out.println("Get mail");
+	}
+}
